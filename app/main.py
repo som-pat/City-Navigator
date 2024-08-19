aab1221852822459abb92dfee028c1fc0f2e469d
sompat <53874321+som-pat@users.noreply.github.com>
2024-08-19 15:33:22 +0530

    Docker and HTML
    -Added docker support for other files
    -Added Initial map  and HTML page, css

--- app/main.py
+++ app/main.py
@@ -14,7 +14,26 @@ import datetime
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'Hello World'}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+
+
+@app.get('/',response_class=HTMLResponse)
+async def initial_map(request:Request):
+    return templates.TemplateResponse("overlay.html",{
+        "request":request
+    })
+
+@app.get('/momo')
+async def et_tu_momo():
+    return{'Hello World'}
